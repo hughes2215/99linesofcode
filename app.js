89f54d7bc7f03df5b2ef7a904268b7ac8ae3a238
hughes2215 <hughes2215@gmail.com>
2018-06-06 12:22:03 -0500

    should be finished

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 let friends = ["Chris", "Bart", "Frank", "Curtis", "Fred"];
 
 for (let b = 0; b < friends.length; b++) {
-console.log(friends[b]);
-}
+for (a = 99; a > 0; a--) {
+  if (a > 1) {
+  console.log(  a + " lines of code in the file " + a + " lines of code; " + friends[b] + " strikes one out, clears it all out, " + a + " lines of code in the file")
+ }
 
-//for (a = 99; a > 0; a--) {
-  //if (a > 1) {
-    //console.log(
-     // a +
-      //  " lines of code in the file" +
-    //    a +
-     //   "lines of code;" +
-     //   friends +
-       // " John strikes one out, clears it all out, " +
-        //a +
-        //"lines of code in the file"
-   // );
- // }
-//}
+  
+}
+if(a=1){
+  console.log( a +" line of code in the file " + a + " line of code in file " + friends[b] + "  strikes one out, clears it all out, no more lines of code in the file ");
+}
+}
