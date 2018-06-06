1bd59bfa40685b79f3c9af2ee8c54d292dad4c29
hughes2215 <hughes2215@gmail.com>
2018-06-06 11:30:07 -0500

    figuring the loop out

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-var friends = ['Chris','Bart','Frank', 'Curtis' , 'Fred'];
- 
+let friends = ["Chris", "Bart", "Frank", "Curtis", "Fred"];
 
-for (var a = 99; a > 0; a--)
-{
-    console.log(a);
-    if(friends[a]== 'Chris'){
-      console.log('friends')
-    }
+for (let b = 0; b < friends.length; b++) {
+console.log(friends[b]);
 }
+
+//for (a = 99; a > 0; a--) {
+  //if (a > 1) {
+    //console.log(
+     // a +
+      //  " lines of code in the file" +
+    //    a +
+     //   "lines of code;" +
+     //   friends +
+       // " John strikes one out, clears it all out, " +
+        //a +
+        //"lines of code in the file"
+   // );
+ // }
+//}
